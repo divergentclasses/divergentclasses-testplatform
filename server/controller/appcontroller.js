d6336b2b29fd30788d2ae6cc92731e972a12497c
divergentclasses <devanshgoelai@gmail.com>
2024-09-13 09:30:17 +0530

    Update appcontroller.js

--- server/controller/appcontroller.js
+++ server/controller/appcontroller.js
@@ -14,8 +14,8 @@ const Result = require('../model/result');
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     host: 'smtp.gmail.com',
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.USER,
         pass: process.env.PASS
